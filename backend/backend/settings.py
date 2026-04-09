7a75ab3522c8314a2981ec5cacc42a69b4f9d7da
Marcel Cieśliński <cieslinski.marcel@gmail.com>
2026-04-09 19:54:32 +0200

    Fix: add STORAGES configuration for WhiteNoise to serve Unfold static files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -146,6 +146,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # --- Custom App Settings ---
 
@@ -171,4 +181,3 @@ UNFOLD = {
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
